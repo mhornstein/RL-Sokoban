4666c05bd327ad89a053322f08d4a6fb31a54ac1
maor hornstein <mor.h100@gmail.com>
2023-09-19 07:59:48 +0300

    Rename DQN3 -> DQN

--- experimenter.py
+++ experimenter.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from DQN3 import dqn
+from DQN import dqn
 from EnvWrapper import EnvWrapper
 from experiment_config import *
 from reports_util import log_training_process, log_train_loss, create_report
