829f2ad374715f937a473e7808029b5f73bb5d9c
maor hornstein <mor.h100@gmail.com>
2023-09-19 08:20:23 +0300

    Update indentation

--- DQN.py
+++ DQN.py
@@ -1,31 +1,30 @@
 import torch
-import copy
 import random
 import torch.nn as nn
 from torch.optim import SGD
 from collections import namedtuple, deque
 
 class QNN(nn.Module):
-  "DNN to approximate the Q-function"
-  def __init__(self):
-    super(QNN, self).__init__()
-    self.model = torch.nn.Sequential(
-      torch.nn.Conv2d(3, 32, 5),
-      torch.nn.MaxPool2d((2,2)),
-      torch.nn.ReLU(),
-      torch.nn.Conv2d(32,64, 5),
-      torch.nn.MaxPool2d((2,2)),
-      torch.nn.ReLU(),
-      torch.nn.Conv2d(64,128, 5),
-      torch.nn.MaxPool2d((2,2)),
-      torch.nn.ReLU(),
-      torch.nn.Flatten(),
-      torch.nn.Linear(12800, 512),
-      torch.nn.ReLU(),
-      torch.nn.Linear(512, 13))
+    def __init__(self):
+        super(QNN, self).__init__()
+        self.model = torch.nn.Sequential(
+            torch.nn.Conv2d(3, 32, 5),
+            torch.nn.MaxPool2d((2,2)),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(32,64, 5),
+            torch.nn.MaxPool2d((2,2)),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(64,128, 5),
+            torch.nn.MaxPool2d((2,2)),
+            torch.nn.ReLU(),
+            torch.nn.Flatten(),
+            torch.nn.Linear(12800, 512),
+            torch.nn.ReLU(),
+            torch.nn.Linear(512, 13)
+        )
 
-  def forward(self, x):
-    return self.model(x)
+    def forward(self, x):
+        return self.model(x)
 
 def get_state_tensor(state, done=False):
     if done:
@@ -33,29 +32,29 @@ def get_state_tensor(state, done=False):
     return torch.tensor(state, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)
 
 class ReplayMemory(object):
-  "stores past experiences as fifo"
-  def __init__(self, capacity, batch_size):
-    self.batch_size = batch_size
-    self.memory_baffer = deque([], maxlen=capacity)
-    self.transition = namedtuple('T', ('s', 'a', 'ns', 'r'))
+    "stores past experiences as fifo"
+    def __init__(self, capacity, batch_size):
+        self.batch_size = batch_size
+        self.memory_baffer = deque([], maxlen=capacity)
+        self.transition = namedtuple('T', ('s', 'a', 'ns', 'r'))
 
-  def __len__(self):
+    def __len__(self):
         return len(self.memory_baffer)
 
-  def push(self, state, action, next_state, reward):
-    transition = (state, action, next_state, reward)
-    self.memory_baffer.append(transition)
+    def push(self, state, action, next_state, reward):
+        transition = (state, action, next_state, reward)
+        self.memory_baffer.append(transition)
 
-  def sample(self):
-    batch = random.sample(self.memory_baffer, self.batch_size)
-    return self.transition(*zip(*batch))
+    def sample(self):
+        batch = random.sample(self.memory_baffer, self.batch_size)
+        return self.transition(*zip(*batch))
 
 def epsilon_greedy_action(epsilon, state, env, policy):
     if random.random() < epsilon:
-      return torch.tensor([[env.sample_action()]], dtype=torch.long)
+        return torch.tensor([[env.sample_action()]], dtype=torch.long)
     else:
-      with torch.no_grad():
-        return policy(state).max(1)[1].view(1, 1)
+        with torch.no_grad():
+            return policy(state).max(1)[1].view(1, 1)
 
 def update_target(policy, target):
     target.load_state_dict(policy.state_dict())
@@ -63,7 +62,7 @@ def update_target(policy, target):
 def update_epsilon(epsilon, decay):
     epsilon *= decay
     if epsilon < 0.05:
-      epsilon = 0.05
+        epsilon = 0.05
     return epsilon
 
 def update_model(buffer, policy, target, batch_size, discount_factor, optimizer):
@@ -83,7 +82,7 @@ def update_model(buffer, policy, target, batch_size, discount_factor, optimizer)
     # the estimates with the values produced by the policy network
     next_q_values = torch.zeros(batch_size)
     with torch.no_grad():
-      next_q_values[non_final_mask] = target(next_state_batch).max(1)[0]
+        next_q_values[non_final_mask] = target(next_state_batch).max(1)[0]
 
     # Compute the target Q-values
     target_q_values = (next_q_values * discount_factor) + reward_batch
@@ -110,37 +109,37 @@ def dqn(env, num_episodes, batch_size, gamma, ep_decay, epsilon,
 
     losses, rewards, episodes_steps = [], [], []
     for i in range(1, num_episodes+1):
-      print("\nEpisode: ", i)
-      state = get_state_tensor(env.reset())
-      done = False
-      episode_reward = 0
-      episode_loss = 0
-      num_steps = 1
-      done_count = 0
-      while not done and num_steps <= steps_cutoff:
-        print(num_steps, end = " ")
-        action = epsilon_greedy_action(epsilon, state, env, policy)
-        epsilon = update_epsilon(epsilon, ep_decay)
-        next_state, reward, done, info = env.step(action.item())
-        next_state = get_state_tensor(next_state, done)
-        episode_reward += reward
-        reward = torch.tensor([reward])
-        buffer.push(state, action, next_state, reward)
-        state = next_state
-        if len(buffer) >= batch_size and num_steps % train_action_value_freq_update == 0:
-          loss = update_model(buffer, policy, target, batch_size, gamma, optimizer)
-          episode_loss += loss
-        num_steps += 1
+        print("\nEpisode: ", i)
+        state = get_state_tensor(env.reset())
+        done = False
+        episode_reward = 0
+        episode_loss = 0
+        num_steps = 1
+        done_count = 0
+        while not done and num_steps <= steps_cutoff:
+            print(num_steps, end = " ")
+            action = epsilon_greedy_action(epsilon, state, env, policy)
+            epsilon = update_epsilon(epsilon, ep_decay)
+            next_state, reward, done, info = env.step(action.item())
+            next_state = get_state_tensor(next_state, done)
+            episode_reward += reward
+            reward = torch.tensor([reward])
+            buffer.push(state, action, next_state, reward)
+            state = next_state
+            if len(buffer) >= batch_size and num_steps % train_action_value_freq_update == 0:
+                loss = update_model(buffer, policy, target, batch_size, gamma, optimizer)
+                episode_loss += loss
+            num_steps += 1
 
-      rewards.append(episode_reward)
-      losses.append(episode_loss / num_steps)
-      episodes_steps.append(num_steps)
+        rewards.append(episode_reward)
+        losses.append(episode_loss / num_steps)
+        episodes_steps.append(num_steps)
 
-      if i % target_freq_update == 0:
-        update_target(policy, target)
+        if i % target_freq_update == 0:
+            update_target(policy, target)
 
-      if done:
-          done_count += 1
+        if done:
+            done_count += 1
 
       # save the MidWay parameters to show
       # if num_episodes / 2 == i:
